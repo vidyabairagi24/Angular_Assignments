443bfc8d132d8933a95a8a5b898ea9cba8f0f2ad
vidyabairagi24 <125753825+vidyabairagi24@users.noreply.github.com>
2023-05-11 18:57:16 +0530

    Add files via upload

---
+++ Assignment 13/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
